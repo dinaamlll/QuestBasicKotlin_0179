41b2b1ee49cba3501a2ef541b3be6b457cc7fe2e
dinaamlll <dinamaliaa17@gmail.com>
2024-10-12 21:53:49 +0700

    Menambahkan contoh List (List read-only dan List Mutable) beserta operasi menambah, menghapus, dan mengubah elemen

---
+++ app/src/main/java/com/example/questbasickotlin_0179/Collections.kt
@@ -0,0 +1,34 @@
+package com.example.questbasickotlin_0179
+
+// List adalah Kumpulan data yang disusun secara berurutan dari
+// List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List Read Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
+
+fun ContohList(){
+    println("==List==")
+    //List Read-Only
+    val readOnlyAbjad = listOf("A","B","C")
+    println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle","Square","Triangle")
+    println(shape)
+
+    // Menambahkan data ke dalam list mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Menghapus data dari list mutable
+    shape.remove("Triangle")
+    println(shape)
+
+    // Mengubah data di dalam list mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    // List Read Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
