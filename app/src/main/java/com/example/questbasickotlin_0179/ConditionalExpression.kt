a1f7ebe74465a848f23179cd031352770e776228
dinaamlll <dinamaliaa17@gmail.com>
2024-10-12 22:23:14 +0700

    Menambahkan contoh Try-Catch

--- app/src/main/java/com/example/questbasickotlin_0179/ConditionalExpression.kt
+++ app/src/main/java/com/example/questbasickotlin_0179/ConditionalExpression.kt
@@ -27,4 +27,13 @@ fun ConditionalStatement() {
         else -> println("Invalid Day")
     }
 
+    // Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+
 }
