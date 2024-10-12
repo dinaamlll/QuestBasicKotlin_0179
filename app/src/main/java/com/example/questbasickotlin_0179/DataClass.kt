26fcc16982dea59b07784300c449c255d847f681
dinaamlll <dinamaliaa17@gmail.com>
2024-10-12 23:10:53 +0700

    Menampilkan fungsi equals() untuk membandingkan objek

--- app/src/main/java/com/example/questbasickotlin_0179/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_0179/DataClass.kt
@@ -18,4 +18,8 @@ fun main () {
     val data = DataClass(1, "indra@gmail.com")
     println(data)
 
+//Fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) // false
+
 }
