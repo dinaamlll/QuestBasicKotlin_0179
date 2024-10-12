c2bb4a8d41cf5d2a2e88aa398ed236b887fa680a
dinaamlll <dinamaliaa17@gmail.com>
2024-10-12 23:37:13 +0700

    Menambahkan contoh fungsi untuk mengubah string menjadi huruf kapital

---
+++ app/src/main/java/com/example/questbasickotlin_0179/LambdaFunction.kt
@@ -0,0 +1,10 @@
+package com.example.questbasickotlin_0179
+
+//Lambda Functions
+//Lamda Functions adalah fungsi yang tidak memiliki nama
+//Lambda Functions biasanya digunakan untuk membuat fungsi yang sederhana
+//Lambda Functions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
