3fa4458adba891293ca09f14d7b1c7012974ba17
dinaamlll <dinamaliaa17@gmail.com>
2024-10-12 23:21:38 +0700

    Menambahkan contoh fungsi tanpa parameter dan dengan parameter

---
+++ app/src/main/java/com/example/questbasickotlin_0179/Functions.kt
@@ -0,0 +1,13 @@
+package com.example.questbasickotlin_0179
+
+
+fun withoutParameter(){
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String){
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!=")
+}
